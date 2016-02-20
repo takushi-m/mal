b78e496c11e2bc7b4d4c4efe58c0434ff3ae3da0
MIKI Takushi <mtakushi@MIKI-no-MacBook.local>
2016-02-20 19:37:15 +0900

    add step0_repl.py

---
+++ mypython/step0_repl.py
@@ -0,0 +1,26 @@
+import sys
+import readline
+
+def READ(line):
+    return line
+
+def EVAL(line):
+    return line
+
+def PRINT(line):
+    return line
+
+def rep(input):
+    x = READ(input)
+    y = EVAL(x)
+    z = PRINT(y)
+    return z
+
+
+while True:
+    try:
+        line = input("user> ")
+        output = rep(line)
+        print(output)
+    except Exception as e:
+        break
